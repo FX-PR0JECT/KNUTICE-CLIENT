4d9980002dc5cc4b7bafcf9ed8a577097746e5c1
ccconac <qwerpoiu91@naver.com>
2024-05-18 18:22:59 +0900

    :sparkles: feat: implement academic notice

--- knutice/src/components/Notice/NoticeList.tsx
+++ knutice/src/components/Notice/NoticeList.tsx
@@ -1,5 +1,9 @@
 'use client';
 
+import { useSearchParams } from 'next/navigation';
+import { useState, useEffect } from 'react';
+
+import { API_URL } from '@/services/api';
 import { DefaultImage } from '@/components';
 import {
   CardWrapper,
@@ -13,27 +17,68 @@ import {
   NoticeCardList,
 } from '@/styles/Notice/NoticeList';
 
+interface INotice {
+  departName: string;
+  nttId: number;
+  registrationDate: string;
+  title: string;
+}
+
+interface INoticeCard {
+  notice: INotice;
+  selectedTab: string;
+}
+
 const NoticeList = () => {
+  const params = useSearchParams();
+  const [notices, setNotices] = useState([]);
+  const selectedTab = params.get('tab') || 'academic';
+
+  useEffect(() => {
+    const fetchNotices = async () => {
+      let url = `${API_URL}/`;
+
+      switch (selectedTab) {
+        case 'academic':
+          url += 'academicNews?page=0&size=18';
+          break;
+        default:
+          return;
+      }
+
+      const response = await fetch(url, { cache: 'no-store' });
+      const data = await response.json();
+
+      setNotices(data.data);
+    };
+
+    fetchNotices();
+  }, [selectedTab]);
+
   return (
     <NoticeCardList>
-      <NoticeCard />
-      <NoticeCard />
-      <NoticeCard />
+      {notices.map((notice: INotice) => (
+        <NoticeCard key={notice.nttId} selectedTab={selectedTab} notice={notice} />
+      ))}
     </NoticeCardList>
   );
 };
 
-const NoticeCard = () => {
+const NoticeCard = ({ notice, selectedTab }: INoticeCard) => {
+  const department = (selectedTab: string) => {
+    if (selectedTab === 'academic') return '#학사공지사항 #acadmic';
+  };
+
   return (
     <CardWrapper>
       <LinkItem href="/notice">
         <CardItem>
           <DefaultImage />
           <Notice>
-            <Title>공지 제목 제대로 생략되는지 확인하기 위한 긴 텍스트글</Title>
-            <Department>학사과</Department>
-            <Classification>#일반공지</Classification>
-            <RegistrationDate>2024년 5월 14일</RegistrationDate>
+            <Title>{notice.title}</Title>
+            <Department>{notice.departName}</Department>
+            <Classification>{department(selectedTab)}</Classification>
+            <RegistrationDate>{notice.registrationDate}</RegistrationDate>
           </Notice>
         </CardItem>
       </LinkItem>
