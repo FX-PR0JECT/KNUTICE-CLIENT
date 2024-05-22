2d4641fb114a8b3ba88f615e49473825b41d1358
ccconac <qwerpoiu91@naver.com>
2024-05-22 10:35:57 +0900

    :wrench: chore: add hex color in theme object

---
+++ knutice/src/styles/theme.ts
@@ -0,0 +1,45 @@
+export const THEME = {
+  dark: {
+    backgroundColor: '#000',
+
+    // Main
+    color: '#e3e3e3',
+    borderColor: '#ffffffc8',
+
+    // Navbar
+    borderBottom: '#2f2f2f',
+
+    // NoticeList
+    cardItemBg: '#222224',
+
+    // Tabs
+    tabFontColor: '#000',
+    tabBgColor: '#333333',
+
+    hoverTabBg: '#eee',
+
+    selectedTabBg: '#eee',
+    selectedTabColor: '#000',
+  },
+
+  light: {
+    backgroundColor: '#fff',
+
+    // Main
+    color: '#000',
+    borderColor: '#2f2f2f',
+
+    // Navbar
+    borderBottom: '#eee',
+
+    // NoticeList
+    cardItemBg: '#fff',
+
+    // Tabs
+    tabFontColor: '#000',
+    tabBgColor: '#eee',
+
+    selectedTabBg: '#000',
+    selectedTabColor: '#e3e3e3',
+  },
+};
