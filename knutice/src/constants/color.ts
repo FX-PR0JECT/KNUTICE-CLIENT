af03779d91aba7ad3e03c8650e9dd546aec2d87c
ccconac <qwerpoiu91@naver.com>
2024-05-22 14:19:35 +0900

    :wrench: chore: create color code constant object

---
+++ knutice/src/constants/color.ts
@@ -0,0 +1,32 @@
+export const COLORS = {
+  // theme
+  'bg-dark': '#000',
+  'bg-light': '#fff',
+
+  'font-dark': '#e3e3e3',
+  'font-light': '#000',
+
+  'border-dark': '#ffffffc8',
+  'border-light': '#2f2f2f',
+  'border-bottom-dark': '#2f2f2f',
+  'border-bottom-light': '#eee',
+
+  'card-bg-dark': '#222224',
+  'card-bg-light': '#fff',
+
+  'tab-bg-dark': '#333333',
+  'tab-bg-light': '#eee',
+
+  's-tab-bg-dark': '#eee',
+  's-tab-bg-light': '#000',
+  's-tab-color-dark': '#000',
+  's-tab-color-light': '#e3e3e3',
+
+  // colors
+  white: '#fff',
+  'gradient-first': '#ff0048',
+  'gradient-second': '#fcb2c5',
+
+  gray1: '#797979',
+  gray2: '#999',
+};
