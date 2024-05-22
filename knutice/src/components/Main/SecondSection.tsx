883d4fc8ab2fbc9f8e2266cd0de44fafb50f567b
ccconac <qwerpoiu91@naver.com>
2024-05-22 17:09:50 +0900

    :art: style: apply content constants

--- knutice/src/components/Main/SecondSection.tsx
+++ knutice/src/components/Main/SecondSection.tsx
@@ -1,4 +1,5 @@
 import { HContent } from '@/components';
+import { MAIN_CONTENT } from '@/constants/content';
 import {
   SecondSec,
   ContentWrapper,
@@ -42,15 +43,10 @@ const SecondSection = () => {
   return (
     <SecondSec>
       <HContent>
+        <Content title={MAIN_CONTENT.firstTitle} description={MAIN_CONTENT.description} />
         <Content
-          title={`학교 공지만\n한곳에 모아\n빠르고, 쉽게`}
-          description="학교 홈페이지에 있는 어쩌구 엄청 많은 콘텐츠들 어쩌구 딱 공지만 모아서 정리해 줌
-        그래서 우리 어쩌구 어떤 말을 써야 할지 모르겠지만 소개 어쩌구 암튼 그러하다"
-        />
-        <Content
-          title={`학교 공지를\n핸드폰 알림으로\n더 간편하게`}
-          description="학교 홈페이지에 있는 어쩌구 엄청 많은 콘텐츠들 어쩌구 딱 공지만 모아서 정리해 줌
-        그래서 우리 어쩌구 어떤 말을 써야 할지 모르겠지만 소개 어쩌구 암튼 그러하다"
+          title={MAIN_CONTENT.secondTitle}
+          description={MAIN_CONTENT.description}
           align="right"
           imageFirst={true}
         />
