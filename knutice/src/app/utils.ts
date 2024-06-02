b52b801a34b50ccecfbe2e348277335a7558291c
ccconac <qwerpoiu91@naver.com>
2024-06-02 22:59:38 +0900

    :hammer: fix: delete min width of mobile screen

--- knutice/src/app/utils.ts
+++ knutice/src/app/utils.ts
@@ -3,7 +3,7 @@ import { useMediaQuery } from 'react-responsive';
 const getDeviceType = () => {
   const isDesktop = useMediaQuery({ minWidth: 1024 });
   const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1023 });
-  const isMobile = useMediaQuery({ minWidth: 375, maxWidth: 767 });
+  const isMobile = useMediaQuery({ maxWidth: 767 });
 
   if (isDesktop) return 'desktop';
   if (isTablet) return 'tablet';
